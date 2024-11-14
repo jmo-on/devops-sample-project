3fe44a423b7595f7732c8275e58f11d797229e20
Jin Hong Moon <jaydenmoon7@gmail.com>
2024-11-14 07:59:13 -0500

    working react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Tailwind is already imported
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Hi Eli @ AppLovin</h1>
     </div>
   );
 }
